80413df081bd48f425a35bdfe73333bf7b8bce8c
Joel Edwards <joeledwards@gmail.com>
2017-04-07 12:09:09 -0600

    Now sending each handshake command to the server sequentially, awaiting the OK response before sending the next command.

--- src/main/scala/com/aviatainc/seedstream/Main.scala
+++ src/main/scala/com/aviatainc/seedstream/Main.scala
@@ -10,11 +10,27 @@ import scala.util.Success
 import akka.stream.scaladsl.Framing
 import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Sink
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatterBuilder
+import java.time.ZonedDateTime
+import java.time.ZoneId
 
 case class Station(network: String, name: String)
 case class Feed(station: Station, selectors: Seq[String])
 
 object Main extends App {
+  def timestamp = ZonedDateTime.now()
+      .withZoneSameInstant(ZoneId.of("UTC"))
+      .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+  
+  def log(message: String): Unit = {
+    println(s"[$timestamp] $message")
+  }
+  
+  
   def seedStream(): Unit = {
     implicit val system = ActorSystem()
     import system.dispatcher
@@ -29,22 +45,62 @@ object Main extends App {
       Feed(Station("IU", "ANMO"), Seq("BH?"))
     )
     
-    val feedStrings = feeds.map { case Feed(Station(name, network), selectors) =>
-      val selectorsString = selectors.map(s => s"SELECT $s").mkString(SEP)
-      s"STATION $name $network${SEP}$selectorsString${SEP}DATA"
+    val commands: Seq[String] = feeds.flatMap { case Feed(Station(name, network), selectors) =>
+      s"STATION $name $network" +: selectors.map(s => s"SELECT $s")
+    } :+ "END"
+    
+    log(s"Commands:\n${commands.mkString("\n")}")
+    
+    val handshake = commands map { command =>
+      (ByteString(s"$command$SEP"), Promise[ByteString])
+    } toList
+    
+    val commandIterator = handshake.iterator
+    
+    def nextCommand(fail: Option[Throwable]): Unit = {
+      if (commandIterator.hasNext) {
+        val (command, promise) = commandIterator.next
+        
+        fail match {
+          case None => promise.success(command)
+          case Some(cause) => promise.failure(cause)
+        }
+      }
     }
     
-    val handshake = feedStrings.mkString(SEP) + s"${SEP}END${SEP}"
+    // Complete the first future to get the ball rolling
+    nextCommand(None)
     
-    println(s"Handshake:\n$handshake")
+    // Handshake commands out-bound to the server.
+    val source = Source(handshake)
+        .mapAsync[ByteString](1)(_._2.future)
+        .map { cmd =>
+          log(s"Sending command: ${cmd.decodeString("UTF-8")}")
+          cmd
+        }
     
-    val source = Source(ByteString(handshake) :: Nil)
-    val sink = Flow[ByteString]
+    // Records in-bound from the server.
+    val flow = Flow[ByteString]
         .map(record => (record.length, record.decodeString("ascii"), record))
+    
+    val sink = flow
         .to(Sink.foreach { case (len, decoded, raw) =>
-          val recordSummary = if (len <= 40) s":\n$raw\n$decoded" else ""
-          println(s"Received $len byte record${recordSummary}")
+          if (decoded startsWith "ERROR") {
+            // Handle an error with handshake commands
+            log("Command rejected [ERROR]")
+            nextCommand(Some(new Exception("Server rejected command.")))
+          } else if (decoded startsWith "OK") {
+            // Make sure all of the commands sent sequentially
+            log("Command accepted [OK]")
+            nextCommand(None)
+          } else {
+            // Process the record feed
+            log("Processing a record")
+            val recordSummary = if (len <= 40) s":\n$raw\n$decoded" else ""
+            log(s"Received $len byte record${recordSummary}")
+          }
         })
+        
     val connection = Tcp().outgoingConnection("rtserve.iris.washington.edu", 18000)
     
     connection.runWith(source, sink)
